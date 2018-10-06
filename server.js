7aa69d97f9dae2cc98fb0024d17186f833f714e3
Glitch (code-create-todo-schema) <none>
2018-10-06 14:21:22 -0700

    tests work

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const Todo = require('./models');
+const Todo = require('./models/toDo');
 const { PORT, DATABASE_URL } = require('./config');
 
 const seedData = require('./db/todos.json');
 
-const app = express(); 
+const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/v1/todos', (req, res, next) => {
-  Todo.find()
-    .then(todos => res.json(todos.map(todo => todo.serialize())))
-    .catch(next);
+app.get('/todos', (req, res, next) => {
+  Todo.find({})
+    .then(todos => res.send(todos))
+    .catch(err => {
+      console.error(err);
+    });
 });
 
-app.get('/v1/todos/:id', (req, res, next) => {
+app.get('/todos/:id', (req, res, next) => {
   const id = req.params.id;
   Todo.findById(id)
     .then(item => {
       if (item) {
         res.json(item.serialize());
-      } else { 
+      } else {
         next();
       }
-    }) 
+    })
     .catch(next);
 });
 
-app.post('/v1/todos', (req, res, next) => {
+app.post('/todos', (req, res, next) => {
   const { title } = req.body;
 
   /***** Never trust users - validate input *****/
@@ -39,7 +42,7 @@ app.post('/v1/todos', (req, res, next) => {
     err.status = 400;
     return next(err);
   }
-  
+
   // Using promises
   Todo.create({title})
     .then(newItem => {
@@ -48,10 +51,9 @@ app.post('/v1/todos', (req, res, next) => {
         .json(newItem.serialize());
   })
     .catch(next);
-
 });
 
-app.put('/v1/todos/:id', (req, res, next) => {
+app.put('/todos/:id', (req, res, next) => {
   const id = req.params.id;
   /***** Never trust users - validate input *****/
   const updateItem = {};
@@ -79,7 +81,7 @@ app.put('/v1/todos/:id', (req, res, next) => {
     .catch(next);
 });
 
-app.delete('/v1/todos/:id', (req, res, next) => {
+app.delete('/todos/:id', (req, res, next) => {
   const id = req.params.id;
   // Using promises
   Todo.findByIdAndRemove(id)
@@ -111,19 +113,18 @@ app.use(function (err, req, res, next) {
 
 
 if (require.main === module) {
-  console.log('============ SERVER RUNNING ==========')
   mongoose.connect(DATABASE_URL, { useMongoClient: true })
     .then(() => {
-      Todo.insertMany(seedData);  
-    }) 
+      Todo.insertMany(seedData);
+    })
     .catch(err => {
       console.error('ERROR: Mongoose failed to connect! Is the database running?');
       console.error(err);
-    });  
-  
-    app.listen(PORT, function () {
-      console.log('Your app is listening on port ' + this.address().port);
     });
+
+  app.listen(PORT, function () {
+    console.log('Your app is listening on port ' + this.address().port);
+  });
 }
 
 module.exports = app; // Export for testing
--- test/server.test.js
+++ test/server.test.js
@@ -1,5 +1,5 @@
 /* global describe, it, before, after, beforeEach, afterEach */
-
+require('dotenv').config();
 const app = require('../server');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -7,7 +7,7 @@ const chaiSpies = require('chai-spies');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const Todo = require('../models');
+const Todo = require('../models/toDo');
 const seedData = require('../db/todos.json');
 const { DATABASE_URL } = require('../config');
 
@@ -18,14 +18,14 @@ chai.use(chaiSpies);
 
 describe('Todo API:', function () {
 
-  before(function () {  
+  before(function () {
     return mongoose.connect(DATABASE_URL, { useMongoClient: true })
       .catch(err => {
         console.error("ERROR: Mongoose failed to connect! Did you configure DATABASE_URL in the '.env'?");
         console.error(err);
       });
   });
-  
+
   beforeEach(function () {
     return mongoose.connection.dropDatabase()
       .then(() => Todo.insertMany(seedData));
@@ -33,17 +33,17 @@ describe('Todo API:', function () {
 
   afterEach(function () {
     return mongoose.connection.dropDatabase();
-  }); 
+  });
 
   after(function () {
     return mongoose.disconnect();
-  }); 
+  });
 
-  describe('GET /v1/todos', function () {
+  describe('GET /todos', function () {
 
-    it('should respond to GET `/v1/todos` with an array of todos and status 200', function () {
+    it('should respond to GET `/todos` with an array of todos and status 200', function () {
       return chai.request(app)
-        .get('/v1/todos')
+        .get('/todos')
         .then(res => {
           res.should.have.status(200);
           res.should.be.json;
@@ -51,23 +51,22 @@ describe('Todo API:', function () {
           res.body.should.have.length(5);
           res.body.forEach((item) => {
             item.should.be.a('object');
-            item.should.include.keys('id', 'title', 'completed');
+            item.should.include.keys('__v','_id', 'title', 'completed');
           });
         });
     });
 
   });
 
-  describe('GET /v1/todos/:id', function () {
-
-    it('should return correct todo when given an id', function () {
+  describe('GET /v1/todos/:id', function() {
+    it('should return correct todo when given an id', function() {
       let doc;
       return Todo.findOne()
         .then(_doc => {
-          doc = _doc
-          return chai.request(app).get(`/v1/todos/${doc._id}`);
+          doc = _doc;
+          return chai.request(app).get(`/todos/${doc._id}`);
         })
-        .then(function (res) {
+        .then(function(res) {
           res.should.have.status(200);
           res.should.be.json;
           res.body.should.be.an('object');
@@ -75,14 +74,13 @@ describe('Todo API:', function () {
           res.body.id.should.equal(doc.id);
           res.body.title.should.equal(doc.title);
           res.body.completed.should.equal(doc.completed);
-          
         });
     });
 
     it('should respond with a 404 when given an invalid id', function () {
       const spy = chai.spy();
       return chai.request(app)
-        .get('/v1/todos/aaaaaaaaaaaaaaaaaaaaaaaa')
+        .get('/todos/aaaaaaaaaaaaaaaaaaaaaaaa')
         .then(spy)
         .then(() => {
           spy.should.not.have.been.called();
@@ -91,10 +89,10 @@ describe('Todo API:', function () {
           err.response.should.have.status(404);
         });
     });
-    
+
   });
 
-  describe('POST /v1/todos', function () {
+  describe('POST /todos', function () {
 
     it('should create and return a new todo when provided valid data', function () {
       const newItem = {
@@ -102,7 +100,7 @@ describe('Todo API:', function () {
       };
 
       return chai.request(app)
-        .post('/v1/todos')
+        .post('/todos')
         .send(newItem)
         .then(res => {
           res.should.have.status(201);
@@ -111,7 +109,7 @@ describe('Todo API:', function () {
           res.body.should.include.keys('id', 'title', 'completed');
           res.body.title.should.equal(newItem.title);
           res.body.completed.should.equal(false);
-          res.should.have.header('location', `/v1/todos/${res.body.id}`);
+          res.should.have.header('location', `/todos/${res.body.id}`);
         });
     });
 
@@ -121,7 +119,7 @@ describe('Todo API:', function () {
       };
       const spy = chai.spy();
       return chai.request(app)
-        .post('/v1/todos')
+        .post('/todos')
         .send(badItem)
         .then(spy)
         .then(() => {
@@ -134,18 +132,18 @@ describe('Todo API:', function () {
 
   });
 
-  describe('PUT /v1/todos/:id', function () {
+  describe('PUT /todos/:id', function () {
 
     it('should update item when given valid data and an id', function () {
       const item = {
         'title': 'Buy New Dishes'
       };
-      
+
       let doc;
       return Todo.findOne()
         .then(_doc => {
           doc = _doc
-          return chai.request(app).put(`/v1/todos/${doc._id}`).send(item);
+          return chai.request(app).put(`/todos/${doc._id}`).send(item);
         })
         .then(res => {
           res.should.have.status(200);
@@ -162,10 +160,10 @@ describe('Todo API:', function () {
         foobar: 'broken item'
       };
       const spy = chai.spy();
-      
+
       return Todo.findOne()
         .then(doc => {
-          return chai.request(app).put(`/v1/todos/${doc._id}`).send(badItem);
+          return chai.request(app).put(`/todos/${doc._id}`).send(badItem);
         })
         .then(spy)
         .then(() => {
@@ -182,7 +180,7 @@ describe('Todo API:', function () {
       };
       const spy = chai.spy();
       return chai.request(app)
-        .put('/v1/todos/aaaaaaaaaaaaaaaaaaaaaaaa')
+        .put('/todos/aaaaaaaaaaaaaaaaaaaaaaaa')
         .send(item)
         .then(spy)
         .then(() => {
@@ -195,22 +193,22 @@ describe('Todo API:', function () {
 
   });
 
-  describe('DELETE /v1/todos/:id', function () {
-
-    it('should delete an item by id', function () {
+  describe('DELETE /todos/:id', function() {
+    it('should delete an item by id', function() {
       return Todo.findOne()
         .then(doc => {
-          return chai.request(app).delete(`/v1/todos/${doc._id}`);
+          return chai.request(app).delete(`/todos/${doc._id}`);
         })
         .then(res => {
           res.should.have.status(204);
         });
     });
 
-    it('should respond with a 404 for an invalid id', function () {
+    it('should respond with a 404 for an invalid id', function() {
       const spy = chai.spy();
-      return chai.request(app)
-        .delete('/v1/todos/aaaaaaaaaaaaaaaaaaaaaaaa')
+      return chai
+        .request(app)
+        .delete('/todos/aaaaaaaaaaaaaaaaaaaaaaaa')
         .then(spy)
         .then(() => {
           spy.should.not.have.been.called();
@@ -219,7 +217,6 @@ describe('Todo API:', function () {
           err.response.should.have.status(404);
         });
     });
-
   });
 
 });
